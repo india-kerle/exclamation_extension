7154928594af2630371f61a2351e0ab186cd058b
India-Kerle_canoe <india.kerle@canoeintelligence.com>
2026-01-09 13:34:57 +0000

    feat: rewrite content script with allowlist and configurable exclamations

--- content_script.js
+++ content_script.js
@@ -1,90 +1,111 @@
-// Replace period with exclamation points 
+// Exclamation Propagandist - Content Script
+// Replaces sentence-ending periods with exclamation points
 
-function walk(rootNode)
-{
-    // Find all the text nodes in rootNode
-    var walker = document.createTreeWalker(
-        rootNode,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
-    ),
-    node;
+(async function() {
+  // Get current settings
+  const settings = await getSettings();
 
-    // Modify each text node's value
-    while (node = walker.nextNode()) {
-        handleText(node);
-    }
-}
-
-function handleText(textNode) {
-  textNode.nodeValue = replaceText(textNode.nodeValue);
-}
-
-function replaceText(v)
-{
-    v = v.replace(/\.$/, "!!! ");
-    v = v.replace(/\."$/, '!!!" ');
-
-    return v
-}
-
-function remove(s){
-    return s[s.length - 1] === "!" ? s.slice(0, s.length - 1) : s
+  // Check if extension is enabled
+  if (!settings.enabled) {
+    return;
   }
 
-// Returns true if a node should *not* be altered in any way
-function isForbiddenNode(node) {
-    return node.isContentEditable || // DraftJS and many others
-    (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
-    (node.tagName && (node.tagName.toLowerCase() == "textarea" || // Some catch-alls
-                     node.tagName.toLowerCase() == "input"));
-}
+  // Check if current site is allowed
+  const hostname = window.location.hostname.replace(/^www\./, '');
+  if (!isHostnameAllowed(hostname, settings)) {
+    return;
+  }
 
-// The callback used for the document body and title observers
-function observerCallback(mutations) {
-    var i, node;
+  // Build the replacement string
+  const exclamations = '!'.repeat(settings.exclamationCount);
 
-    mutations.forEach(function(mutation) {
-        for (i = 0; i < mutation.addedNodes.length; i++) {
-            node = mutation.addedNodes[i];
-            if (isForbiddenNode(node)) {
-                // Should never operate on user-editable content
-                continue;
-            } else if (node.nodeType === 3) {
-                // Replace the text for text nodes
-                handleText(node);
-            } else {
-                // Otherwise, find text nodes within the given node and replace text
-                walk(node);
-            }
-        }
-    });
-}
+  // Regex for sentence-ending periods
+  // Matches period followed by: whitespace, end of string, or closing punctuation
+  const periodRegex = /\.(?=\s|$|["'"'\)\]])/g;
 
-// Walk the doc (document) body, replace the title, and observe the body and title
-function walkAndObserve(doc) {
-    var docTitle = doc.getElementsByTagName('title')[0],
-    observerConfig = {
-        characterData: true,
-        childList: true,
-        subtree: true
-    },
-    bodyObserver, titleObserver;
+  function replaceText(text) {
+    return text.replace(periodRegex, exclamations);
+  }
 
-    // Do the initial text replacements in the document body and title
-    walk(doc.body);
-
-    doc.title = replaceText(doc.title);
-
-    // Observe the body so that we replace text in any added/modified nodes
-    bodyObserver = new MutationObserver(observerCallback);
-    bodyObserver.observe(doc.body, observerConfig);
-
-    // Observe the title so we can handle any modifications there
-    if (docTitle) {
-        titleObserver = new MutationObserver(observerCallback);
-        titleObserver.observe(docTitle, observerConfig);
+  function handleTextNode(textNode) {
+    const newValue = replaceText(textNode.nodeValue);
+    if (newValue !== textNode.nodeValue) {
+      textNode.nodeValue = newValue;
     }
-}
-walkAndObserve(document);
+  }
+
+  function walkTree(rootNode) {
+    const walker = document.createTreeWalker(
+      rootNode,
+      NodeFilter.SHOW_TEXT,
+      null,
+      false
+    );
+
+    let node;
+    while (node = walker.nextNode()) {
+      if (!isForbiddenNode(node)) {
+        handleTextNode(node);
+      }
+    }
+  }
+
+  function isForbiddenNode(node) {
+    // Don't modify editable content
+    if (node.isContentEditable) return true;
+    if (node.parentNode && node.parentNode.isContentEditable) return true;
+
+    // Don't modify form inputs
+    const parent = node.parentNode;
+    if (parent && parent.tagName) {
+      const tag = parent.tagName.toLowerCase();
+      if (tag === 'textarea' || tag === 'input' || tag === 'script' || tag === 'style' || tag === 'code' || tag === 'pre') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function observerCallback(mutations) {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (isForbiddenNode(node)) return;
+
+        if (node.nodeType === Node.TEXT_NODE) {
+          handleTextNode(node);
+        } else if (node.nodeType === Node.ELEMENT_NODE) {
+          walkTree(node);
+        }
+      });
+    });
+  }
+
+  // Initial replacement
+  walkTree(document.body);
+
+  // Replace in title
+  if (document.title) {
+    document.title = replaceText(document.title);
+  }
+
+  // Observe for dynamic content
+  const observer = new MutationObserver(observerCallback);
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+
+  // Observe title changes
+  const titleElement = document.querySelector('title');
+  if (titleElement) {
+    const titleObserver = new MutationObserver(() => {
+      document.title = replaceText(document.title);
+    });
+    titleObserver.observe(titleElement, {
+      childList: true,
+      characterData: true,
+      subtree: true
+    });
+  }
+})();
